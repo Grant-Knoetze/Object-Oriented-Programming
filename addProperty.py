a7b3bc4601e91bca0aaca249655a9f04565a4ee3
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2021-11-05 21:14:55 +0200

    Remove old code and introduce property to class: Staff

--- addProperty.py
+++ addProperty.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # To demonstrate how properties work we add one to the variable position
 # to ensure that it can only be set to 'Basic' or 'Manager'
 
@@ -41,43 +41,6 @@ class Staff:
         hourlyRate = input(prompt)
         self.pay = int(hours) * int(hourlyRate)
         return self.pay
-=======
-
-class Athlete:
-    def __init__ (self, pDiscipline, pName, pExperiencePoints):
-        self._discipline = pDiscipline
-        self.name = pName
-        self.experiencepoints = pExperiencePoints
-        print('Creating Athlete object')
-
-    def __str__ (self):
-        return "Discipline = %s, Name = %s, ExperiencePoints = %d" %(self._discipline, self.name, self.experiencepoints)
-
-    def calculateExperiencePoints(self):
-        prompt = '\nEnter number of hours exercised for %s: ' % (self.name)
-        hours = input(prompt)
-        prompt = 'Enter the hourly EP for %s: ' %(self.name)
-        hourlyPoints = input(prompt)
-        self.experiencepoints = int(hours)*int(hourlyPoints)
-        return self.experiencepoints
-    
-#Next we add a property for the 'discipline' variable. Illustrating how a property is used to check the value of a change made before it is allowed to occur.
-      
-    @property
-    def discipline(self):
-        print("Getter Method")
-        return self._discipline
-
-    @discipline.setter
-    def discipline(self, value):
-        if value == 'Archer' or value == 'Shot Put':
-            self._discipline == value
-        else:
-            print('Discipline is invalid. No changes made.')
 
 
             
-            
-        
-    
->>>>>>> 137ac9dcd98a7653d8a888a471a584df5d29db17
